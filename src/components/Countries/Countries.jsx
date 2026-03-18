485f9b3c95073f57a546b51927ea0480c97bb8cb
Muktade Mumir <muktadetabassommumir@gmail.com>
2026-03-18 15:01:40 +0600

    country api fetch and made it functional, button toggling using conditional rendering

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,18 +1,35 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../Country/Country';
 import './Countries.css';
 
 const Countries = ({countriesPromise}) => {
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
-    console.log(countries);
+    // console.log(countries);
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
+    const handleVisitedCountries = (country) =>{
+        // console.log('Clicked', country);
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+
+    }
     
     return (
         <div >
             <h1>In the Countries: {countries.length}</h1>
+            <p>Total Country Visited: {visitedCountries.length}</p>
+            <ol>
+                {
+                    visitedCountries.map(country => <li key={country.cca3.cca3}>{country.name.common}</li>)
+                }
+            </ol>
             <div className='countries'>
                 {
-                    countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+                    countries.map(country => <Country
+                         key={country.cca3.cca3} 
+                         country={country}
+                         handleVisitedCountries={handleVisitedCountries}></Country>)
                 }
             </div>
         </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css';
 
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountries}) => {
     const {name, flags, population, area} = country;
 
+    const [visited, setVisited] = useState(false);
+
     const handleVisited = () =>{
-        console.log("Button CLicked");
+        // Basic System
+        // if(visited){
+        //     setVisited(false);
+        // }
+        // else{
+        //     setVisited(true);
+        // }
+
+        // Second system
+        // setVisited(visited ? false : true);
+
+        // Third System
+        setVisited(!visited);
+
+        handleVisitedCountries(country);
     }
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={flags.flags.png} alt="" />
             <h3>Name: {name.common}</h3>
             <p>Population: {population.population}</p>
             <p>Area: {area.area} {area.area > 300000 ? 'Big Country':'Small Country'}</p>
-            <button onClick={handleVisited}>Not Visited</button>
+            <button onClick={handleVisited}>
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
         </div>
     );
 };
